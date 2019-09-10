d267fd3794e6a46eb230335534cd1456a781b574
Daoyuan Yang <u6547564@anu.edu.au>
2019-09-10 17:45:25 +1000

    Project created, but don't know how to edit the files even in Kotlin

---
+++ app/src/main/java/com/example/u6547564/scientificcalculator/Calculator.kt
@@ -0,0 +1,12 @@
+package com.example.u6547564.scientificcalculator
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Calculator : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculator)
+    }
+}
