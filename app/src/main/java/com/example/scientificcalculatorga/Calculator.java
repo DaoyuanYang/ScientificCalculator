481c45eb4c8f1815aa2c59da2e9eb4ab48b8ffab
Daoyuan Yang <u6547564@anu.edu.au>
2019-09-12 14:57:39 +1000

    UI buttons are functional

--- app/src/main/java/com/example/scientificcalculatorga/Calculator.java
+++ app/src/main/java/com/example/scientificcalculatorga/Calculator.java
@@ -3,8 +3,14 @@ package com.example.scientificcalculatorga;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Calculator extends AppCompatActivity {
     private Button buttonDclPt;
@@ -19,9 +25,9 @@ public class Calculator extends AppCompatActivity {
     private Button button8;
     private Button button9;
     private Button buttonEqual;
-    EditText myText = findViewById(R.id.myText);
+    EditText myText;
 
-
+    private List<Button> buttonList = new ArrayList<>();
 
 
     @Override
@@ -29,6 +35,7 @@ public class Calculator extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
 
+        myText = findViewById(R.id.myText);
         buttonDclPt = findViewById(R.id.buttonDclPt);
         button0 = findViewById(R.id.button0);
         button1 = findViewById(R.id.button1);
@@ -42,15 +49,26 @@ public class Calculator extends AppCompatActivity {
         button9 = findViewById(R.id.button9);
         buttonEqual = findViewById(R.id.buttonEqual);
 
+        buttonList = Arrays.asList(buttonEqual,button0,button1,button2,button3,button4,
+                button5,button6,button7,button8,button9,buttonDclPt);
 
+
+        for (final Button butt : buttonList){
+            butt.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    appendOnExpression((String) butt.getText());
+                }
+            });
+        }
     }
 
-    public void appendOnExpression(char ch){
-        if (ch == '+' || ch == '-' || ch == '*' ||ch == '/') {
-            myText.setText(myText.getText().toString() + ch);
+    public void appendOnExpression(String str){
+        if (str == "+" || str == "-" || str == "*" ||str == "/") {
+            myText.setText(myText.getText().toString() + str);
         }
         else{
-            myText.setText(ch);
+            myText.setText(str);
         }
     }
 }
