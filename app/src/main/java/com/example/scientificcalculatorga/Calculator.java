f1872e2c973131807e3f742b78a79b0eddb9b8b0
Daoyuan Yang <u6547564@anu.edu.au>
2019-09-13 14:08:31 +1000

    Equal sign removing editText works fine, resultText shows

--- app/src/main/java/com/example/scientificcalculatorga/Calculator.java
+++ app/src/main/java/com/example/scientificcalculatorga/Calculator.java
@@ -1,3 +1,7 @@
+/**
+ * Inspired by video : https://www.youtube.com/watch?v=EpP6KgJtHTk&t=504s
+ *
+ */
 package com.example.scientificcalculatorga;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -5,9 +9,11 @@ import androidx.arch.core.util.Function;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -15,6 +21,8 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Calculator extends AppCompatActivity {
+    private Boolean isEvaluated = false;
+
     private Button buttonDclPt;
     private Button button0;
     private Button button1;
@@ -28,7 +36,15 @@ public class Calculator extends AppCompatActivity {
     private Button button9;
     private Button buttonEqual;
     private Button buttonFunction;
+    private Button buttonAC;
+
+    private Button buttonDiv;
+    private Button buttonMul;
+    private Button buttonSub;
+    private Button buttonAdd;
+
     EditText myText;
+    TextView resultText;
 
     private List<Button> buttonList = new ArrayList<>();
 
@@ -39,6 +55,8 @@ public class Calculator extends AppCompatActivity {
         setContentView(R.layout.activity_calculator);
 
         myText = findViewById(R.id.myText);
+        resultText = findViewById(R.id.resultText);
+
         buttonDclPt = findViewById(R.id.buttonDclPt);
         button0 = findViewById(R.id.button0);
         button1 = findViewById(R.id.button1);
@@ -52,7 +70,22 @@ public class Calculator extends AppCompatActivity {
         button9 = findViewById(R.id.button9);
         buttonEqual = findViewById(R.id.buttonEqual);
         buttonFunction = findViewById(R.id.buttonFunction);
+        buttonAC = findViewById(R.id.buttonAC);
 
+
+        buttonDiv = findViewById(R.id.buttonDiv);
+        buttonMul = findViewById(R.id.buttonMul);
+        buttonSub = findViewById(R.id.buttonSub);
+        buttonAdd = findViewById(R.id.buttonAdd);
+
+        /**ButtonList contains all the input keys
+         * Used for FUNC.appendOnExpression to decide whether the content needs to be cleared
+         */
+        buttonList = Arrays.asList(buttonEqual,button0,button1,button2,button3,button4,
+                button5,button6,button7,button8,button9,buttonDclPt, buttonAC,
+                buttonDiv, buttonMul, buttonSub, buttonAdd);
+
+        //Set button clickListeners
         buttonFunction.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -60,32 +93,51 @@ public class Calculator extends AppCompatActivity {
             }
         });
 
-        buttonList = Arrays.asList(buttonEqual,button0,button1,button2,button3,button4,
-                button5,button6,button7,button8,button9,buttonDclPt);
-
-
+        /**
+         * Apply all input keys FUNC.appendOnExpression
+         */
         for (final Button butt : buttonList){
             butt.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    appendOnExpression((String) butt.getText());
+                    appendOnExpression(butt.getText().toString());
                 }
             });
         }
     }
 
+    /**
+     * To decide whether to clear the content of calculator text
+     * @param str is keyPress value
+     */
     public void appendOnExpression(String str){
-        if (str == "+" || str == "-" || str == "*" ||str == "/") {
+        if (str.equals("=")) {
+            isEvaluated = true;
+            resultText.setText("ZERO");
+        }
+        else if (str.equals("AC")) myText.setText("");
+        else {
+            if (isEvaluated){
+                isEvaluated = false;
+                myText.setText("");
+            }
             myText.setText(myText.getText().toString() + str);
         }
-        else{
-            myText.setText(str);
-        }
+//        if (str == "+" || str == "-" || str == "*" ||str == "/") {
+//            myText.setText(myText.getText().toString() + str);
+//        }
+//        else{
+//            myText.setText(str);
+//        }
     }
 
     public void jumpToFunction(){
         Intent intent = new Intent(this, FunctionDefining.class)
                 .setFlags(Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT);
         startActivity(intent);
+    }
+
+    public double calculateResult(){
+        return 1.2;
     }
 }
